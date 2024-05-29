55d67153f16de5f81dbbbdf2e715d60566a512e3
everchicas <everchicasp@outllook.com>
2024-05-29 17:17:29 -0600

    add decode

--- app/jwt_manager.py
+++ app/jwt_manager.py
@@ -1,5 +1,8 @@
-from jwt import encode
+from jwt import encode, decode
 
 def create_token(data: dict):
     token: str = encode(payload=data, key="MY_SECRETE_KEY", algorithm="HS256")
     return token
+
+def validate_token(token:str) -> dict:
+    return decode(token,key="MY_SECRETE_KEY", algorithm=["HS256"])
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, body, Path, Query
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
@@ -12,3 +12,14 @@ app = FastAPI(
 class Usuario(BaseModel):
     correo: str
     password: str = Field(min_length=4)
+
+@app.get("/", tags=["Home"])
+def home():
+    return HTMLResponse("<h1>Usando JWT con fast API</h1>")
+
+@app.post("/login", tags=["autheticator"])
+async def login(user:Usuario):
+    if(user.correo=="admin@example.com" and user.password=="1234"):
+        return JSONResponse(content=create_token(user.dict()), status_code=200)
+    else:
+        return JSONResponse(content="Error en autenticación", status_code=400)
